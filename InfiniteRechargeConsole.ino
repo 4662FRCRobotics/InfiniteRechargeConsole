5237d5325d0d7cfc256f6af2e2091277a243b667
FRC4662-DS3 <bytesizedprogramming@gmail.com>
2021-01-28 11:47:05 -0800

    add turn rate and camera servo controls

--- InfiniteRechargeConsole.ino
+++ InfiniteRechargeConsole.ino
@@ -16,7 +16,7 @@
 */
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
 0, 2,
-false, false, false, 
+true, true, false, 
 false, false, false,  
 false, false,
 false, false, false);
@@ -38,16 +38,37 @@ int PosSwIncrement2 = PosSwMax / PosSwSegments;
 int PosSwMidPoint2 = PosSwIncrement / 2;
 int PosSwPosition2 = 0;
 
+int TurnRatePin = A2;
+int TurnRateMax = 1023;
+int TurnRateMin = 0;
+
+int IntakeCameraPin = A3;
+int IntakeCameraMax = 1023;
+int IntakeCameraMin = 0;
+
+String PosPatMap[6][6] = {
+    {"X Auto              ", "X Auto              ", "X Auto              ", "-                   ", "-                   ", "-                   "},
+    {"Shoot + X           ", "Shoot + X           ", "Shoot + X           ", "-                   ", "-                   ", "-                   "},
+    {"Shoot + X           ", "Shoot + X + Wheel   ", "Shoot + X + Wheel   ", "-                   ", "-                   ", "-                   "},
+    {"Shoot + X           ", "Shoot + X           ", "Shoot+Collect+Shoot ", "-                   ", "-                   ", "-                   "},
+    {"-                   ", "-                   ", "-                   ", "-                   ", "-                   ", "-                   "},
+    {"-                   ", "-                   ", "-                   ", "-                   ", "-                   ", "-                   "}
+};
+
 void setup() {
   // put your setup code here, to run once:
 
   Joystick.begin();
+  Joystick.setXAxisRange(0,1023);
+  Joystick.setYAxisRange(0,1023);
+  
   lcd.init();
   lcd.clear();
   lcd.setBacklight(255);
   
 }                                 
-  String strPosSwitchStatement = "V3 2/19/19";
+  String strPosSwitchStatement = "V4 2021 1/18/2021";
+  String strAutoCommand = "";
                                  
 void loop() {
     lcd.home();
@@ -57,7 +78,10 @@ void loop() {
 
     int PosSwPosition = (PosSwValue * PosSwSegments + PosSwMidPoint) / PosSwMax;
     int PosSwPosition2 = (PosSwValue2 * PosSwSegments2 + PosSwMidPoint2) / PosSwMax2;
-    
+
+    int TurnRate = map(analogRead(TurnRatePin), 0, 1023, TurnRateMin, TurnRateMax);
+    int CameraAngle = map(analogRead(IntakeCameraPin), 0, 1023, IntakeCameraMin, IntakeCameraMax);
+   
     lcd.print("StartPos:");
     lcd.setCursor(0,1);
     lcd.print("Pattern:");
@@ -99,23 +123,31 @@ void loop() {
             strStartPos = "three  ";
             break;
         case 3:
-            strStartPos2 = "four   ";
+            strStartPos = "four   ";
             break;
         case 4:
-            strStartPos2 = "five    ";
+            strStartPos = "five    ";
             break;
         case 5:
-            strStartPos2 = "Six     ";     
+            strStartPos = "Six     ";     
         default:
             strStartPos = "unknown";
     }
+
+    strAutoCommand = PosPatMap[PosSwPosition2][PosSwPosition];
 
    lcd.setCursor(10,0);
    lcd.print(strStartPos);
    lcd.setCursor(10,1);
    lcd.print(strStartPos2);
+   lcd.setCursor(0, 2);
+   lcd.print("Auto Pattern:");
+   lcd.setCursor(0, 3);
+   lcd.print(strAutoCommand);
 
    Joystick.setHatSwitch(0, PosSwPosition*45);
    Joystick.setHatSwitch(1, PosSwPosition2*45);
+   Joystick.setXAxis(TurnRate);
+   Joystick.setYAxis(CameraAngle);
   
 }
